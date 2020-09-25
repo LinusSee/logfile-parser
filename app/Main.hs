92c7d64e8610793cb3963970a0a520555926b3ff
Linus <linussee@web.de>
2020-09-25 19:06:23 +0200

    feat: simple date parser

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,15 @@
+{-# LANGUAGE FlexibleContexts #-}
+
 module Main where
 
+import qualified Text.Parsec as Parsec
 import Lib
 
+parse rule text = Parsec.parse rule "(source)" text
+dateInput :: String
+dateInput = "2011-11-23 random continuation"
+
+
 main :: IO ()
-main = someFunc
+main = putStrLn (show $ parse dateParser dateInput)
+--main = someFunc
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,23 @@
 module Lib
     ( someFunc
+    , dateParser
     ) where
+
+import qualified Text.Parsec as Parsec
+
+dateParser :: Parsec.Parsec String () String
+dateParser = do
+  year <- Parsec.count 4 Parsec.digit
+  sep1 <- Parsec.char '-'
+  month <- Parsec.count 2 Parsec.digit
+  sep2 <- Parsec.char '-'
+  day <- Parsec.count 2 Parsec.digit
+  return (year ++ [sep1] ++ month ++ [sep2] ++ day)
+
+--toParseThemAll :: Parsec.Parsec String () (String, String)
+--toParseThemAll = do
+
+
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
