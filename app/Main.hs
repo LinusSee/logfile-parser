09ab1564f9835d4942634416fe42c8d985b8c1e9
Linus <linussee@web.de>
2020-09-25 20:00:06 +0200

    wip: string to date and loglevel parsing

--- app/Main.hs
+++ app/Main.hs
@@ -3,13 +3,16 @@
 module Main where
 
 import qualified Text.Parsec as Parsec
+import Data.Time
 import Lib
 
 parse rule text = Parsec.parse rule "(source)" text
 dateInput :: String
-dateInput = "2011-11-23 random continuation"
+dateInput = "2011-11-23 17:08:00,769 random continuation"
 
 
 main :: IO ()
-main = putStrLn (show $ parse dateParser dateInput)
+--main = putStrLn (show $ parse dateTimeParser dateInput)
+main = putStrLn (show $ parse loglevelParser "WARN")
+--main = putStrLn $ show (parseTimeM True defaultTimeLocale "%Y-%m-%d %H:%M:%S,%q" ("2019-08-31 17:08:00,769" ++ replicate 9 '0') :: Maybe UTCTime)
 --main = someFunc
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,8 @@
 module Lib
     ( someFunc
     , dateParser
+    , dateTimeParser
+    , loglevelParser
     ) where
 
 import qualified Text.Parsec as Parsec
@@ -13,6 +15,28 @@ dateParser = do
   sep2 <- Parsec.char '-'
   day <- Parsec.count 2 Parsec.digit
   return (year ++ [sep1] ++ month ++ [sep2] ++ day)
+
+timeParser :: Parsec.Parsec String () String
+timeParser = do
+  hours <- Parsec.count 2 Parsec.digit
+  sep1 <- Parsec.char ':'
+  minutes <- Parsec.count 2 Parsec.digit
+  sep2 <- Parsec.char ':'
+  seconds <- Parsec.count 2 Parsec.digit
+  sep3 <- Parsec.char ','
+  ms <- Parsec.count 3 Parsec.digit
+  return (hours ++ [sep1] ++ minutes ++ [sep2] ++ seconds ++ [sep3] ++ ms)
+
+dateTimeParser :: Parsec.Parsec String () String
+dateTimeParser = do
+  date <- dateParser
+  Parsec.char ' '
+  time <- timeParser
+  return (date ++ [' '] ++ time)
+
+loglevelParser :: Parsec.Parsec String () String
+loglevelParser = do
+  Parsec.choice (map Parsec.string ["DEBUG", "WARN"])
 
 --toParseThemAll :: Parsec.Parsec String () (String, String)
 --toParseThemAll = do
