9329c3bd681380abf91db7b960c61c8901813d79
Linus <linussee@web.de>
2020-11-13 20:16:42 +0100

    feat: working date parser

--- parser-rest/src/LogfileParsing.hs
+++ parser-rest/src/LogfileParsing.hs
@@ -6,6 +6,7 @@ module LogfileParsing
 
 import qualified Text.Parsec as Parsec
 import Control.Monad.IO.Class (liftIO)
+import Data.List
 import Data.Time
 import CustomParsers (ElementaryParser(..), ParsingRequest(..), ParsingResponse(..))
 
@@ -23,7 +24,7 @@ applyParser target parser =
       parse (applyTime pattern) target
 
     Date _ pattern ->
-      parse applyDate target -- TODO: Incorrect
+      parse (applyDate pattern) target
 
     Characters _ chars ->
       parse (applyCharacters chars) target
@@ -48,16 +49,85 @@ applyTime format = do
       return $ TimeResponse (result ++ "-asString")
 
 
-applyDate :: Parsec.Parsec String () ParsingResponse
-applyDate = do
-  result <- Parsec.string "target"
-  return $ DateResponse result
+applyDate :: String -> Parsec.Parsec String () ParsingResponse
+applyDate format = do
+  result <- datePatternToParsers format
+  -- "%H:%M:%S.%q" and replicate 9 '0'
+  let day = parseTimeM False defaultTimeLocale "%Y%m%d" result :: Maybe Day
+  case day of
+    Just parsedDay ->
+      return $ DateResponse (show parsedDay)
+
+    Nothing ->
+      return $ DateResponse (result ++ "-asString")
+
+
+  -- result <- Parsec.string "target"
+  -- return $ DateResponse result
 
 
 applyCharacters :: String -> Parsec.Parsec String () ParsingResponse
 applyCharacters target = do
   result <- Parsec.string target
   return $ CharactersResponse result
+
+
+datePatternToParsers :: String -> Parsec.Parsec String () String
+datePatternToParsers pattern =
+  case take 2 pattern of
+    "MM" ->
+      case take 2 $ drop 3 pattern of
+        "DD" -> do
+          month <- monthParser
+          _ <- Parsec.char $ head (drop 2 pattern)
+          day <- dayParser
+          _ <- Parsec.char $ head (drop 5 pattern)
+          year <- yearParser
+          return (year ++ month ++ day)
+
+        _ -> do
+          month <- monthParser
+          _ <- Parsec.char $ head (drop 2 pattern)
+          year <- yearParser
+          _ <- Parsec.char $ head (drop 7 pattern)
+          day <- dayParser
+          return (year ++ month ++ day)
+
+    "DD" ->
+      case take 2 $ drop 3 pattern of
+        "MM" -> do
+          day <- dayParser
+          _ <- Parsec.char $ head (drop 2 pattern)
+          month <- monthParser
+          _ <- Parsec.char $ head (drop 5 pattern)
+          year <- yearParser
+          return (year ++ month ++ day)
+
+        _ -> do
+          day <- dayParser
+          _ <- Parsec.char $ head (drop 2 pattern)
+          year <- yearParser
+          _ <- Parsec.char $ head (drop 7 pattern)
+          month <- monthParser
+          return (year ++ month ++ day)
+
+    _ ->
+      case take 2 $ drop 5 pattern of
+        "MM" -> do
+          year <- yearParser
+          _ <- Parsec.char $ head (drop 4 pattern)
+          month <- monthParser
+          _ <- Parsec.char $ head (drop 7 pattern)
+          day <- dayParser
+          return (year ++ month ++ day)
+
+        "DD" -> do
+          year <- yearParser
+          _ <- Parsec.char $ head (drop 4 pattern)
+          day <- dayParser
+          _ <- Parsec.char $ head (drop 7 pattern)
+          month <- monthParser
+          return (year ++ month ++ day)
 
 
 timePatternToParsers :: String -> Parsec.Parsec String () String
@@ -92,3 +162,21 @@ minuteParser = Parsec.choice $ reverse
     (map (Parsec.try . Parsec.string . show) [1..59])
     ++ [Parsec.string "1"]
   --Parsec.choice $ reverse (map (Parsec.string . show) [1..59])
+
+yearParser :: Parsec.Parsec String () String
+yearParser = do
+  start <- Parsec.choice $ map (Parsec.string . show) [1..9]
+  rest <- Parsec.count 3 (Parsec.choice $ map (Parsec.string . show) [0..9])
+  return (start ++ intercalate "" rest)
+
+
+monthParser :: Parsec.Parsec String () String
+monthParser = do
+  result <- Parsec.count 2 (Parsec.choice $ map (Parsec.string . show) [0..9])
+  return $ intercalate "" result
+
+
+dayParser :: Parsec.Parsec String () String
+dayParser = do
+  result <- Parsec.count 2 (Parsec.choice $ map (Parsec.string . show) [0..9])
+  return $ intercalate "" result
