07c8dfd93768551cc5f4dc87ff5897fbce323ec6
Linus <linussee@web.de>
2021-01-11 20:30:25 +0100

    test: validateTarget quickcheck properties (#31)

--- parser-rest/test/business-logic/ValidationSpec.hs
+++ parser-rest/test/business-logic/ValidationSpec.hs
@@ -3,8 +3,8 @@ module ValidationSpec
 ) where
 
 import qualified Data.Either as Either
--- import qualified Data.Either.Combinators as EitherComb
 import Test.Hspec
+import Test.Hspec.QuickCheck (prop)
 
 import Validation
 import ValidationModels
@@ -17,19 +17,35 @@ spec =
           it "returns a <Right> type" $ do
               validateTarget "a valid string" `shouldSatisfy` Either.isRight
 
+
           it "returns a <Valid> type containing the target string if it is not null" $ do
-              let result = validateTarget "a valid string"
+              fromRight' (validateTarget "a valid string") `shouldBe` Just "a valid string"
 
-              -- TODO: Maybe refactor somehow? e.g. fail in case statement
 
-              Either.isRight result `shouldBe` True
-              map fromValid (Either.rights [result]) `shouldBe` ["a valid string"]
 
       context "when provided with an invalid input (empty string)" $ do
           it "returns a <Left> type if the target string is empty" $ do
               validateTarget "" `shouldSatisfy` Either.isLeft
 
+
           it "returns a correct <ValidationError> if target string is empty" $ do
               validateTarget "" `shouldBe` (Left $ ValidationError
                                               (FieldValidation "target")
                                               "The target string to parse must not be empty.")
+
+
+
+      context "properties" $ do
+          prop "returns a <ValidationError> for target == null and <Valid> containing the target otherwise" $
+            \target -> if null target
+                       then validateTarget target `shouldBe` (Left $ ValidationError
+                                                       (FieldValidation "target")
+                                                       "The target string to parse must not be empty.")
+                       else fromRight' (validateTarget target) `shouldBe` Just target
+
+
+
+
+fromRight' :: (Show a, Eq a) => Either ValidationError (Valid a) -> Maybe a
+fromRight' (Right val) = Just (fromValid val)
+fromRight' _ = Nothing
