c622da3ac6adbb3981d3f195baad5f9672ce69cc
LinusSee <linussee@web.de>
2021-01-21 23:00:04 +0100

    test: create time pattern generator (#31)

--- parser-rest/test/business-logic/ElementaryParsingSpec.hs
+++ parser-rest/test/business-logic/ElementaryParsingSpec.hs
@@ -2,7 +2,7 @@ module ElementaryParsingSpec
 ( spec
 ) where
 
-import Data.List (isPrefixOf)
+import Data.List (delete, isPrefixOf)
 import Data.Either (isLeft)
 import Test.Hspec
 import Test.Hspec.QuickCheck (prop)
@@ -17,7 +17,7 @@ spec :: Spec
 spec = do
     -- TODO: Improve readability/descriptions (ugly edge cases)
     describe "applyParser" $ do
-        context "when provided with a valid target value and an elementary parser" $ do
+        context "when provided with a valid target value and an OneOf parser" $ do
             it "returns the first match even if another would fit" $ do
                 let parser = OneOf "name" ["matchNormal", "other", "matchNormalExtended"]
                 applyParser "matchNormalExtended" parser `shouldBe` Right (OneOfResult "matchNormal")
@@ -32,16 +32,23 @@ spec = do
                     applyParser val parser `shouldBe` Right (OneOfResult val)
 
 
-        context "when provided with an invalid target value and an elementary parser" $ do
+        context "when provided with an invalid target value and an OneOf parser" $ do
             prop "does not match a value if it isn't in a OneOf parsers list (and no smaller matches exist)" $ do
                 \name -> QC.forAll (QC.listOf1 nonEmptyString) $ \values -> QC.forAll (QC.elements values) $ \val -> do
                     -- Remove elements that start the same as the target element
-                    let validValue = \valueToCheck -> not (isPrefixOf val valueToCheck)
+                    let validValue = \valueToCheck -> not (isPrefixOf valueToCheck val)
                     let validValues = filter validValue values
                     let parser = (OneOf name validValues)
 
                     applyParser val parser `shouldSatisfy` isLeft
 
+
+        context "when provided with a valid target value and a Time parser" $ do
+            it "returns a time by matching the pattern" $ do
+              pending
+
+
+        -- Use the newly learned knowledge about generators to generate patterns
 
 
     describe "chooseParser" $ do
@@ -54,3 +61,15 @@ spec = do
 
 nonEmptyString :: QC.Gen String
 nonEmptyString = QC.listOf1 QC.arbitrary
+
+
+validTimePattern :: QC.Gen String
+validTimePattern = do
+    first <- QC.elements timePatternBlocks
+    let second = head (delete first timePatternBlocks)
+    separator <- QC.arbitrary
+    return $ first ++ (separator : second)
+
+
+timePatternBlocks :: [String]
+timePatternBlocks = ["HH", "MM"]
