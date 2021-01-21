12e177045db69fd0b0b03a45b8deb2abd4644336
LinusSee <linussee@web.de>
2021-01-21 21:24:32 +0100

    test: apply oneOf parser (#31)

--- parser-rest/test/business-logic/ElementaryParsingSpec.hs
+++ parser-rest/test/business-logic/ElementaryParsingSpec.hs
@@ -2,6 +2,8 @@ module ElementaryParsingSpec
 ( spec
 ) where
 
+import Data.List (isPrefixOf)
+import Data.Either (isLeft)
 import Test.Hspec
 import Test.Hspec.QuickCheck (prop)
 import qualified Test.QuickCheck as QC
@@ -13,18 +15,32 @@ import ElementaryParsing
 
 spec :: Spec
 spec = do
+    -- TODO: Improve readability/descriptions (ugly edge cases)
     describe "applyParser" $ do
         context "when provided with a valid target value and an elementary parser" $ do
-            prop "matches a value if it is in a OneOf parsers list" $ do
-                \name -> QC.forAll (QC.listOf1 nonEmptyString) $ \values -> QC.forAll (QC.elements values) $ \val ->
-                    applyParser val (OneOf name values) `shouldBe` Right (OneOfResult val)
-                -- \name -> QC.forAll (listOf1 values) $ \val ->
-                --     applyParser val (OneOf name values) `shouldBe` Right (OneOfResult val)
-                -- \name values -> do
-                --     let parser = OneOf name values
-                --     -- map (\val -> applyParser val parser `shouldBe` (Right (OneOfResult val))) values
-                --     applyParser (head values) (OneOf name values) `shouldBe` Right (OneOfResult (head values))
-                --     -- (applyParser "asdf" (OneOf "check" ["asdf"])) `shouldBe` Right (OneOfResult "asdf")
+            it "returns the first match even if another would fit" $ do
+                let parser = OneOf "name" ["matchNormal", "other", "matchNormalExtended"]
+                applyParser "matchNormalExtended" parser `shouldBe` Right (OneOfResult "matchNormal")
+
+            prop "matches a value if it is in a OneOf parsers list (with smaller matches removed)" $ do
+                \name -> QC.forAll (QC.listOf1 nonEmptyString) $ \values -> QC.forAll (QC.elements values) $ \val -> do
+                    -- Remove elements that start the same as the target element
+                    let validValue = \valueToCheck -> not (isPrefixOf valueToCheck val) || valueToCheck == val
+                    let validValues = filter validValue values
+                    let parser = (OneOf name validValues)
+
+                    applyParser val parser `shouldBe` Right (OneOfResult val)
+
+
+        context "when provided with an invalid target value and an elementary parser" $ do
+            prop "does not match a value if it isn't in a OneOf parsers list (and no smaller matches exist)" $ do
+                \name -> QC.forAll (QC.listOf1 nonEmptyString) $ \values -> QC.forAll (QC.elements values) $ \val -> do
+                    -- Remove elements that start the same as the target element
+                    let validValue = \valueToCheck -> not (isPrefixOf val valueToCheck)
+                    let validValues = filter validValue values
+                    let parser = (OneOf name validValues)
+
+                    applyParser val parser `shouldSatisfy` isLeft
 
 
 
