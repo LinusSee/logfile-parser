ebcefa328fff8ca867139f4cbe708962ad5c3e17
LinusSee <linussee@web.de>
2021-02-04 18:34:00 +0100

    test: add parsers as testdata (#32)

--- parser-rest/test/rest/ApiSpec.hs
+++ parser-rest/test/rest/ApiSpec.hs
@@ -6,16 +6,23 @@ module ApiSpec
 import Test.Hspec
 
 import System.Directory (createDirectoryIfMissing, removeDirectoryRecursive)
+import qualified Data.Text as T
+
+import CustomParsers (ElementaryParser (..))
+import ElementaryParserFileDb (save) -- For initialising data
+import qualified Configs as Configs
 
 
 createDbFiles :: IO ()
 createDbFiles = do
-    createDirectoryIfMissing True "assets/test_db"
-    writeFile "assets/test_db/parsers.txt" $ show ([] :: [Int])
+    createDirectoryIfMissing True dbBasePath
+    writeFile (dbBasePath ++ elementaryParsersDbName) $ show ([] :: [Int])
+    writeFile (dbBasePath ++ logfileParsersDbName) $ show ([] :: [Int])
 
 
 clearDbFiles :: IO ()
-clearDbFiles = removeDirectoryRecursive "assets/test_db"
+clearDbFiles = removeDirectoryRecursive dbBasePath
+
 
 
 spec :: Spec
@@ -24,3 +31,60 @@ spec = before_ createDbFiles $
          describe "api" $ do
            it "returns" $ do
              pending
+
+
+
+
+createElementaryParsers :: IO ()
+createElementaryParsers = mapM_ (save fileDbConfig) parsers
+
+  where dbPath = dbBasePath ++ elementaryParsersDbName
+        parsers = [ oneOfParser
+                  , timeParser
+                  , matchUntilIncludedParser
+                  , matchUntilExcludedParser
+                  , matchUntilEndParser
+                  ]
+
+
+fileDbConfig :: Configs.FileDbConfig
+fileDbConfig = Configs.FileDbConfig
+                  (T.pack $ dbBasePath ++ elementaryParsersDbName)
+                  (T.pack $ dbBasePath ++ logfileParsersDbName)
+
+oneOfParser :: ElementaryParser
+oneOfParser = OneOf "loglevelParser" ["INFO", "INCIDENT", "ERROR"]
+
+timeParser :: ElementaryParser
+timeParser = Time "dashedTimeParser" "HH-MM"
+
+matchUntilIncludedParser :: ElementaryParser
+matchUntilIncludedParser = MatchUntilIncluded "untilCorrelationId" "<correlationId>"
+
+matchUntilExcludedParser :: ElementaryParser
+matchUntilExcludedParser = MatchUntilExcluded "correlationId" "</correlationId>"
+
+matchUntilEndParser :: ElementaryParser
+matchUntilEndParser = MatchUntilEnd "matchUntilEnd"
+
+-- data Config = Config
+--   { fileDbConfig :: FileDbConfig
+--   , apiConfig :: APIConfig
+--   }
+--
+--
+--
+-- data FileDbConfig = FileDbConfig
+--   { elementaryParserPath :: T.Text
+--   , logfileParserPath :: T.Text
+--   } deriving (Show)
+
+
+dbBasePath :: FilePath
+dbBasePath = "assets/test_db"
+
+elementaryParsersDbName :: String
+elementaryParsersDbName = "/parsers.txt"
+
+logfileParsersDbName :: String
+logfileParsersDbName = "/logfile_parsers.txt"
