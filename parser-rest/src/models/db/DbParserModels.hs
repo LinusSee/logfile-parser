edc41ab0103a0273fe8a96cba4216ddb06c9ba4f
LinusSee <linussee@web.de>
2021-04-16 19:06:20 +0200

    feat: add new db models (#38)

---
+++ parser-rest/src/models/db/DbParserModels.hs
@@ -0,0 +1,47 @@
+module CustomParsers
+(
+) where
+
+
+data ElementaryParser =
+  ElementaryParser { name :: String,
+                   , options :: ParsingOptions,
+                   , type :: ParserType
+                   }
+  deriving (Show, Read, Eq)
+
+
+data ParserType =
+    OneOf [String]
+  | Time TimePattern
+  | Date DatePattern
+  | Characters String
+  | MatchUntilIncluded String
+  | MatchUntilExcluded String
+  | MatchFor Int
+  | MatchUntilEnd
+  deriving (Show, Read, Eq)
+
+type TimePattern = String
+type DatePattern = String
+
+
+data ParsingOptions =
+  ParsingOptions { keepResult :: Bool
+                 }
+  deriving (Show, Read, Eq)
+
+
+data NamedElementaryParser =
+  NamedElementaryParser { name :: String
+                        , parser :: ElementaryParser
+                        }
+  deriving (Show, Read, Eq)
+
+
+
+data LogfileParser =
+  LogfileParser { name :: String
+                , namedParsers :: [ElementaryParser]
+                }
+  deriving (Show, Read, Eq)
