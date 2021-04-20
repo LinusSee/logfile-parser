f114f17c0447e256feec234a3b3a87ef379b06fc
LinusSee <linussee@web.de>
2021-04-20 14:26:53 +0200

    feat: add business logic models (#38)

---
+++ parser-rest/src/models/business-logic/BusinessLogicModels.hs
@@ -0,0 +1,82 @@
+{-# LANGUAGE DuplicateRecordFields #-}
+
+module BusinessLogicModels
+(
+) where
+
+import Data.Time (TimeOfDay, Day)
+
+
+data ElementaryParser =
+  Elementaryparser { name :: String
+                   , options :: ParsingOptions
+                   , parserType :: ParserType
+                   }
+  deriving (Show, Read, Eq)
+
+
+data ParsingOptions =
+  ParsingOptions { keepResult :: Bool
+                 }
+  deriving (Show, Read, Eq)
+
+
+data ParserType =
+    OneOf [String]
+  | Time TimePattern
+  | Date DatePattern
+  | Characters String
+  | MatchUntilIncluded String
+  | MatchUntilExcluded String
+  | MatchFor Int
+  | MatchUntilEnd
+  deriving (Show, Read, Eq)
+
+type TimePattern = String
+type DatePattern = String
+
+
+
+data LogfileParser =
+  LogfileParser { name :: String
+                , namedParsers :: [NamedElementaryParser]
+                }
+  deriving (Show, Read, Eq)
+
+
+data NamedElementaryParser =
+  NamedElementaryParser { name :: String
+                        , parser :: ElementaryParser
+                        }
+  deriving (Show, Read, Eq)
+
+
+
+data ElementaryParsingResult =
+  ElementaryParsingResult { name :: String
+                          , resultType :: ParsingResultType
+                          }
+  deriving (Show, Read, Eq)
+
+data ParsingResultType =
+    OneOfResult String
+  | TimeResult TimeOfDay
+  | DateResult Day
+  | CharactersResult String
+  | MatchUntilIncludedResult String
+  | MatchUntilExcludedResult String
+  | MatchForResult String
+  | MatchUntilEndResult String
+  | ParsingError String
+  deriving (Show, Read, Eq)
+
+
+data LogfileParsingResult =
+  LogfileParsingResult [[NamedElementaryParsingResult]]
+  deriving (Show, Read, Eq)
+
+data NamedElementaryParsingResult =
+    NamedElementaryParsingResult { name :: String
+                                 , result :: ElementaryParsingResult
+                                 }
+    deriving (Show, Read, Eq)
